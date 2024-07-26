655aa167b7790bcaba4015330c455445e4283d21
Aru778 <40577253+Aru778@users.noreply.github.com>
2024-07-26 13:03:49 +0530

    Create new.sh

---
+++ new.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+env=${env_name}
+execute=${myboolean}
+echo "Hello!! Artisan-Tek!!"
+echo "Executing in ${env}--Environment"
+if [[ $execute == "true" ]]; then
+echo "Executing in ${env}--Environment"
+echo "Executing the task"
+else
+echo "skipping the task for ${env}--Environment"
+fi
